9f5a9852024927406bb3e1641d6fee5260e2aa22
saljs <salskare@gmail.com>
2015-08-27 00:21:22 -0500

    added conf file parsing

--- htcpcpd.c
+++ htcpcpd.c
@@ -17,19 +17,25 @@ pid_t* pidchild;
 int* sock1;
 int* sock2;
 
-void sig_handler(int signo)
+//set user-set variables
+int time_to_brew = 1800;
+int port = 80;
+int relay_pin = 0;
+int button_pin = 2;
+int led_pin = 3;
+
+void sig_handler(int signo)  //terminate gracefully
 {
     if (signo == SIGINT || signo == SIGTERM)
     {
         kill(*pidchild, SIGQUIT);
         close(*sock1);
         close(*sock2);
-        digitalWrite(3, LOW);
-        digitalWrite(0, LOW);
+        digitalWrite(led_pin, LOW);
+        digitalWrite(relay_pin, LOW);
         exit(0);
     }
 }
-
 
 typedef struct
 {
@@ -46,7 +52,7 @@ int brew(void)
         potinfo.ready = 0;
         potinfo.lastbrew = time(NULL);
         //brew coffee
-        digitalWrite(0, HIGH);
+        digitalWrite(relay_pin, HIGH);
         return 0;
     }
     return 1;
@@ -100,46 +106,98 @@ char* handleHeaders(char *request)
     }
 }
 
+void setVars(char* var, char* val)
+{
+    if(strstr(var, "time_to_brew") != NULL)
+    {
+        time_to_brew = atoi(val);
+    }
+    else if(strstr(var, "port") != NULL)
+    {
+        port = atoi(val);
+    }
+    else if(strstr(var, "relay_pin") != NULL)
+    {
+        relay_pin = atoi(val);
+    }
+    else if(strstr(var, "button_pin") != NULL)
+    {
+        button_pin = atoi(val);
+    }
+    else if(strstr(var, "led_pin") != NULL)
+    {
+        led_pin = atoi(val);
+    }
+}
+
+
 int main(int argc, char *argv[])
 {
     printf("HTCPCP/1.0 server starting\n");
-    sleep(30);
+
+    //ignore conf file if it doesn't exist
+    if(access( "/etc/htcpcp.conf", F_OK ) != -1)
+    {
+        //read fom conf file
+        FILE* confile = fopen("/etc/htcpcp.conf", "r");
+        char tmp[255];
+        while(fgets(&tmp, sizeof(tmp), confile) != NULL)
+        {
+            //ignore comments
+            if(tmp[0] == '#')
+            {
+                continue;
+            }
+            else
+            {
+                //get the variable being set
+                char *var = strtok(tmp, "=");
+                //get the value it's being set to
+                char *val = strtok(tmp, "=");
+                //set it
+                setVars(var, val);
+            }
+        }
+    }
+    
+    sleep(30); //dirty hack to make sure WiFi is on before the program starts when using Systemd unit file
     potinfo.ready = 1;
     potinfo.lastbrew = time(NULL);
-    int sockfd, newsockfd, portno;
+    int sockfd, newsockfd;
     sock1 = &sockfd;
     sock2 = &newsockfd;
     socklen_t clilen;
     char buffer[256];
     struct sockaddr_in serv_addr, cli_addr;
     int n;
-    if (argc < 2)
+    if(argc > 2)
     {
-        portno = 80;
-    }
-    else
-    {
-        portno = atoi(argv[1]);
+        //if the port is specified via command line, override default and conf file
+        port = atoi(argv[1]);
     }
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
+    {
         error("ERROR opening socket");
+    }
     bzero((char *) &serv_addr, sizeof(serv_addr));
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,
-             sizeof(serv_addr)) < 0)
+    serv_addr.sin_port = htons(port);
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+    {
         error("ERROR on binding");
+    }
 
     signal(SIGINT, sig_handler);
     signal(SIGCHLD,SIG_IGN);
+    //setup the GPIO pins 
     wiringPiSetup();
-    pinMode (0, OUTPUT);
-    pinMode (2, INPUT);
-    pinMode (3, OUTPUT);
-    digitalWrite(3, HIGH);
+    pinMode (relay_pin, OUTPUT);
+    pinMode (button_pin, INPUT);
+    pinMode (led_pin, OUTPUT);
+    digitalWrite(led_pin, HIGH);  //turn on the indicator LED
     //threading time
     pid_t childPID;
     int var_lcl = 0;
@@ -171,50 +229,50 @@ int main(int argc, char *argv[])
                 if(potcpy.ready == 0)
                 {
                     //pot is brewing
-                    if(time(NULL) - potcpy.lastbrew >= 1800)
+                    if(time(NULL) - potcpy.lastbrew >= time_to_brew)
                     {
                         //half hour
-                        digitalWrite(0, LOW);
+                        digitalWrite(relay_pin, LOW);
                     }
-                    if(time(NULL) - potcpy.lastbrew >= 1800 && time(NULL) > blink)
+                    if(time(NULL) - potcpy.lastbrew >= time_to_brew && time(NULL) > blink)
                     {
-                        if(digitalRead(3) == HIGH)
+                        if(digitalRead(led_pin) == HIGH)
                         {
-                            digitalWrite(3, LOW);
+                            digitalWrite(led_pin, LOW);
                         }
-                        else if(digitalRead(3) == LOW)
+                        else if(digitalRead(led_pin) == LOW)
                         {
-                            digitalWrite(3, HIGH);
+                            digitalWrite(led_pin, HIGH);
                         }
                         blink = time(NULL);
                     }
                     else if(time(NULL) > blink + 4)
                     {
-                        if(digitalRead(3) == HIGH)
+                        if(digitalRead(led_pin) == HIGH)
                         {
-                            digitalWrite(3, LOW);
+                            digitalWrite(led_pin, LOW);
                         }
-                        else if(digitalRead(3) == LOW)
+                        else if(digitalRead(led_pin) == LOW)
                         {
-                            digitalWrite(3, HIGH);
+                            digitalWrite(led_pin, HIGH);
                         }
                         blink = time(NULL);
                     }
                 }
                 else
                 {
-                    digitalWrite(3, HIGH);
+                    digitalWrite(led_pin, HIGH);
                 }
 
-                if(digitalRead(2) == HIGH)
+                if(digitalRead(button_pin) == HIGH)
                 {
                     sleep(3);
-                    if(potcpy.ready == 0 && time(NULL) - potcpy.lastbrew >= 1800)
+                    if(potcpy.ready == 0 && time(NULL) - potcpy.lastbrew >= time_to_brew)
                     {
                         potcpy.ready = 1;
                         write(potRed[1], &potcpy, sizeof(pot));
                     }
-                    else if(digitalRead(2) == HIGH)
+                    else if(digitalRead(button_pin) == HIGH)
                     {
                         //shutdown
                         system("/usr/bin/poweroff");
@@ -224,7 +282,7 @@ int main(int argc, char *argv[])
                         potcpy.ready = 0;
                         potcpy.lastbrew = time(NULL);
                         //brew coffee
-                        digitalWrite(0, HIGH);
+                        digitalWrite(relay_pin, HIGH);
                         write(potRed[1], &potcpy, sizeof(pot));
                     }
                 }
@@ -240,18 +298,24 @@ int main(int argc, char *argv[])
 
                 listen(sockfd,5);
                 clilen = sizeof(cli_addr);
-                newsockfd = accept(sockfd,
-                                   (struct sockaddr *) &cli_addr,
-                                   &clilen);
+                newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
                 if (newsockfd < 0)
+                {
                     error("ERROR on accept");
+                }
                 bzero(buffer,256);
                 n = read(newsockfd,buffer,255);
-                if (n < 0) error("ERROR reading from socket");
+                if (n < 0)
+                {
+                    error("ERROR reading from socket");
+                }
                 read(potRed[0], &potinfo, sizeof(pot));
                 char* returnHead = handleHeaders(buffer);
                 n = write(newsockfd,returnHead,strlen(returnHead));
-                if (n < 0) error("ERROR writing to socket");
+                if (n < 0) 
+                {
+                    error("ERROR writing to socket");
+                }
 
                 write(fd[1], &potinfo, sizeof(pot));
             }
@@ -264,5 +328,7 @@ int main(int argc, char *argv[])
 
     close(newsockfd);
     close(sockfd);
+    digitalWrite(led_pin, LOW);
+    digitalWrite(button_pin, LOW);
     return 0;
 }
